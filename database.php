6a24a232aa2e86160a743e3091d128cdc9848084
batkhai290499 <khainb@beetsoft.com.vn>
2020-05-07 00:46:23 +0700

    test

--- database.php
+++ database.php
@@ -36,4 +36,24 @@ function query ($sql)
 }
 ?>
 
+// $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=GWCourses', 'postgres', '');
+		// echo "done";
 
+		$db = parse_url(getenv("DATABASE_URL"));
+		$pdo = new PDO("pgsql:" . sprintf(
+		    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+		    $db["host"],
+		    $db["port"],
+		    $db["user"],
+		    $db["pass"],
+		    ltrim($db["path"], "/")
+		));
+		
+
+
+		$sql = "SELECT * FROM registercourse";
+		$stmt = $pdo->prepare($sql);
+		//Thiết lập kiểu dữ liệu trả về
+		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$stmt->execute();
+		$resultSet = $stmt->fetchAll();
