2b8cafd5c80bb493f03fba33f84e1ce91972e387
batkhai290499 <khainb@beetsoft.com.vn>
2020-05-06 23:07:19 +0700

    test

--- index.php
+++ index.php
@@ -1,2 +1,43 @@
-<?php echo 'HelloWorld';
- ?>
+<?php  
+		// $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=GWCourses', 'postgres', '');
+		// echo "done";
+
+		$db = parse_url(getenv("DATABASE_URL"));
+		$pdo = new PDO("pgsql:" . sprintf(
+		    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+		    $db["host"],
+		    $db["port"],
+		    $db["user"],
+		    $db["pass"],
+		    ltrim($db["path"], "/")
+		));
+		
+
+
+		$sql = "SELECT * FROM account";
+		$stmt = $pdo->prepare($sql);
+		//Thiết lập kiểu dữ liệu trả về
+		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$stmt->execute();
+		$resultSet = $stmt->fetchAll();
+				
+	?>
+
+<ul>
+		<?php  
+			foreach ($resultSet as $row) {
+			echo '<li>' .
+				$row['username'] . ' --' . $row['password'] . ' --' .$row['roleid'] 
+				. '</li>';
+			}
+		?>
+	</ul>
+
+
+	<li>
+		
+		<div class=""><?= $row['username'] ?></div>
+		<div class=""><?= $row['password'] ?></div>
+		<div class=""><?= $row['roleid'] ?></div>
+		<div class="clear-both"></div>
+	</li>
