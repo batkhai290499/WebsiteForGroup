c71428e6005e72dfb3c55dd3ce355f8e9d1c1afe
batkhai290499 <khainb@beetsoft.com.vn>
2020-05-06 23:34:47 +0700

    test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 //tiến hành kiểm tra là người dùng đã đăng nhập hay chưa
 //nếu chưa, chuyển hướng người dùng ra lại trang đăng nhập
 if (!isset($_SESSION['username'])) {
-  header('Location: ./isLogin/login.php');
+  header('Location: login.php');
 }
 ?>
 <!DOCTYPE html>
